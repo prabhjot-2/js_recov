6fa20b2ccafbc2f59258507d14715037713e64a1
prabhjot-2 <prabh1146@gmail.com>
2024-05-07 23:58:33 +0530

    data types

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use Strict"// means we are write new JS version 
+    // nowdays default this is done
+
+//alert(3+3) not gives anas beacuse we using node js not browser
+
+//number => 2 tp power 53
+//bigint
+// string => true/ false
+// null=> standalone value
+//undefined => means hje koi value nhi assign hoi
+//symbol => unique
+
+//object
+
+console.log(typeof null);
+//it gives type of Object 
+// and when we try to get type of undefined it gives
+//undefined
