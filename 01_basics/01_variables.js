5066184ae748e482f9aeebcc52adc4ce11fab3e0
prabhjot-2 <prabh1146@gmail.com>
2024-05-07 23:33:20 +0530

    about variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId= 12344;
+let accountEmail="abc@gmail.com";
+var acconuntPassword="1234556";
+let accountState;
+// var is not used much because scope issue used more 
+//let and const 
+// let used when defined variable is changed and xonst used when 
+//that is non-changeable 
+console.table([accountId, acconuntPassword, accountEmail, accountState]);
