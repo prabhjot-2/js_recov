69477e593f3c24119236fcb92c0ae10b9c2d1967
prabhjot-2 <prabh1146@gmail.com>
2024-05-08 00:16:56 +0530

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+let score="33abs"
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber= Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+// jo vale ham dege =>  convert ho k kay ata hai
+
+//"33" => 33
+//"33abc"=> NaN
+// true=> 1 ; false=> 0;
+// null =>0
+//undefined ==> NaN 
+
+let isLoggedIn="prabh"
+
+let booleanisLoggedIn= Boolean(isLoggedIn)
+console.log(booleanisLoggedIn)
+
+//1 => true ; 0=> false
+//"" =>false
+// "prabh"=> true
+
+let someNumber=21
+
+let stringconvert= String(someNumber)
+console.log(stringconvert)
+console.log( typeof stringconvert)
+
