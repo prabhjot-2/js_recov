0e1fa0656939412b44521c97f81013e38df20553
prabhjot-2 <prabh1146@gmail.com>
2024-05-09 00:20:05 +0530

    dataTypes Summary

---
+++ datatypesSummmary.js
@@ -0,0 +1,10 @@
+//Primitive (call by value)
+
+// 7 types: String, Number, Boolean, null, undefined, BigInt
+//          Symbol
+
+// javascript is dynamic or Static:?
+
+
+//Reference(Non Primitive)
+// Array Objects Functions
