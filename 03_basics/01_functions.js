23145ccbe6d228166df7060eb892dc650cefa586
prabhjot-2 <prabh1146@gmail.com>
2024-05-18 00:13:28 +0530

    Functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,22 @@
+// function first(){
+
+// } syntax
+
+function add(number1, number2){
+    console.log(number1+number2);
+}
+add(3,4);
+
+function add(number1, number2){
+    // let res=number1+number2;
+    // return res;
+    return number1+number2
+}
+
+const res=add(4,5);
+
+console.log("result:" ,res);
+
+// jab ham function me kuch pass nhi karte to ata hai undefined
+// agar ham predefined value de de  parameter me to vohi print karega jab tak null de rhe agar value pass kar diye to vo value use karega na ki predefined vali
+
