1e2f6cb3737e9e97b9ef491b896b82974d76f904
prabhjot-2 <prabh1146@gmail.com>
2024-05-18 20:17:06 +0530

    more about functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -20,3 +20,25 @@ console.log("result:" ,res);
 // jab ham function me kuch pass nhi karte to ata hai undefined
 // agar ham predefined value de de  parameter me to vohi print karega jab tak null de rhe agar value pass kar diye to vo value use karega na ki predefined vali
 
+// jab hame pta ho ki kitne parameter honge functions me 
+// function calculateCartPrice(...num1){
+//     return num1
+// }
+
+function calculateCartPrice(val1,val2,...num1){// j eda ho jave te first and second goes to val1, val2 rest num1
+    return num1; 
+}
+console.log(calculateCartPrice(200,300,400,222))
+
+
+const user={
+    username:"Prabh",
+    prices:199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+    // e dhyan rakhna paina ki key jo den daye shi de rhe k nhi 
+    // nhi te condtions likhni painia 
+}
+handleObject(user) 
