64a5fdf289b9829bd9e43fcecf6a4df52222fecb
prabhjot-2 <prabh1146@gmail.com>
2024-05-22 17:10:41 +0530

    some new operators and more info

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,18 @@
+// truthy and falsy value means bina value means vo value eyi bina condition check kite onu trye ja falsse manya janda hai
+
+// FALSY values: false,0, -0, BigInt 0n, "", null , undefined, NAN
+// enke elava jo bhi hai vo sab truthy values
+
+// Truthy values confuse karn valia:
+//  "0", 'false', " ", [], {}, function(){}
+
+
+//  Nulish Coalescing Operator(??): null undefined
+//e vekh ki variable nu null ja undefined na chal jave e odod use hunda jado koi database to kuj fetch karde 
+
+let val1;
+val1= null?? 10;// ene 10 value onu de deni
+
+
+//Terniary operator
+// condition ? true: false
