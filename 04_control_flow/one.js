ccf673ee8a58aa1542a30e8031e0a20f80c61e0b
prabhjot-2 <prabh1146@gmail.com>
2024-05-22 16:52:26 +0530

    if else

---
+++ 04_control_flow/one.js
@@ -0,0 +1,18 @@
+//if 
+if (condition) {
+    
+}//condition true hai te brcakets de ander chal javega j false te nhi javega
+//=== te check karda k value te data type same hai k nhi
+//== e sirf value check krda 2=="2" gives true but 2==="2" gives false
+
+if (condition) {
+    
+} else {
+    
+}
+// true hovega te if vala block run hovega nhi te else vala
+
+//basic if else if
+
+// && gives true j dono condition true hon (cond1 && cond2)
+// || e true hunda j koi v conditio true hove (cond1|| cond2)
