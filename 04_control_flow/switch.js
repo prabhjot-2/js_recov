21a7aeda13b2e74bb0ca59edbffe135174acaf7e
prabhjot-2 <prabh1146@gmail.com>
2024-05-22 16:57:13 +0530

    about switch statement

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,13 @@
+switch (key) {
+    case value:
+        
+        break;
+    case value:
+        
+        break;
+
+    default:
+        break;
+}// basic syntax
+
+// agar kisi v case ch break nal likhya te baki code sara execute hovega except default vala
