6cf3f15ab672a50c3653d73d9c94a1a59c2cbde8
prabhjot-2 <prabh1146@gmail.com>
2024-05-22 23:31:02 +0530

    about loops mainly for loop

---
+++ 05_iterations(loops)/one.js
@@ -0,0 +1,11 @@
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// } basic syntax
+
+//  break and continue
+// break ki krda jithe lagande us to baad oo nhi chalda or or scope to bhar aa janda jive switch statement ch kita c
+// te continue us nu ik baar nhi run karn dinda us to agli vari run kar dinde o
+// same as in c++;
+// continue khende chlo ik bar skip kardo te break khenda ki nhi nhi sab kuch band kar do
+
