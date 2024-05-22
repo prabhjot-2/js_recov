3c63dc102916e1b1ffad7d640027deb4d48dae78
prabhjot-2 <prabh1146@gmail.com>
2024-05-22 23:47:38 +0530

    more about loops(while and do-while)

---
+++ 05_iterations(loops)/two.js
@@ -0,0 +1,9 @@
+// while (condition) {
+    
+// } basic syntax
+// ide hunda ki phela condition check hundi baad ch kAM hunda
+
+// do {
+    
+// } while (condition); basic syntax of do-while loop
+// ide hunda ki phela kam hunda baad ch condition check hundi
